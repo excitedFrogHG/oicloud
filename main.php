efc2eb8bbb3e6e35eeef11af7845c38a9f8aca41
翁晨皓 <wch20020727@gmail.com>
2018-04-21 00:37:59 +0800

    登录表单优化

--- main.php
+++ main.php
@@ -3,6 +3,7 @@
 <head>
 <meta charset="utf-8">
 <link href="inc/global.css" type="text/css" rel="stylesheet" >
+<link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <script>
 	function displayForm(divid){
 		document.getElementById(divid).style.display="block";
@@ -12,6 +13,7 @@
 		document.getElementById(divid).style.display="none";
 		document.getElementById('bkg').style.display="none";
 	}
+	
 </script>
 <title>linkbin</title>
 </head>
@@ -22,7 +24,19 @@
 		<span style="font-size: 60px;">OICloud</span>
 	</div>
 	<div class="login_form" id="lg">
-		<span>hehehe</span>
+		<form id="lgForm">
+			<div class="input-group">
+				<span class="input-group-addon">用户名：</span>
+				<input class="form-control" type="text" id="lgForm_usr">
+			</div>
+				<br>
+			<div class="input-group">
+				<span class="input-group-addon">密码：</span>
+				<input class="form-control" type="password" id="lgForm_psw">
+			</div>
+			<br>
+			<button class="btn btn-success" type="button" onClick="chkLoginForm();">Log in</button>
+		</form>
 		<br>
 		<span onClick="hideForm('lg');" class="ll">关闭窗口</span>
 	</div>
